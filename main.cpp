e8f5a06ec318c75bdfc7f6d4d312a8b1e4e28688
aleksander <aleksander.mozetic@gmail.com>
2019-10-01 14:59:47 +0200

    Latest commit

--- main.cpp
+++ main.cpp
@@ -137,6 +137,46 @@ int run_once(const Callback& callback) {
 }
 // _________________________________________________________________________________________________________________
 
+void draw(const int& x, std::ostream& out, size_t position) {
+    out << std::string(position, ' ') << x << std::endl;
+}
+
+class object_t {
+public:
+    object_t(const int& x) : self_(std::make_unique<int_model_t>(x)) { std::cout << "ctor" << std::endl; }
+
+    object_t(const object_t& x) : self_(std::make_unique<int_model_t>(*x.self_)) {}
+    object_t(object_t&&) noexcept = default;
+    object_t& operator=(const object_t& x) { return *this = object_t(x); }
+    object_t& operator=(object_t&&) noexcept = default;
+
+    friend void draw(const object_t& x, std::ostream& out, size_t position) {
+        x.self_->draw_(out, position);
+    }
+
+private:
+    struct int_model_t {
+        int_model_t(const int& x) : data_(x) {}
+        void draw_(std::ostream& out, size_t position) const {
+            draw(data_, out, position);
+        }
+        int data_;
+    };
+
+    std::unique_ptr<int_model_t> self_;
+};
+
+using document_t = std::vector<object_t>;
+
+void draw(const document_t& x, std::ostream& out, size_t position) {
+    out << std::string(position, ' ') << "<document>" << std::endl;
+    for (const auto& e : x) draw(e, out, position);
+    out << std::string(position, ' ') << "</document>" << std::endl;
+}
+// _________________________________________________________________________________________________________________
+
+
+
 
 
 int main(int argc, char *argv[])
