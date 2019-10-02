ffba0fb861030accc34141963ede64975a0dd646
aleksander <aleksander.mozetic@gmail.com>
2019-10-02 20:45:10 +0200

    Latest commit

--- main.cpp
+++ main.cpp
@@ -137,16 +137,27 @@ int run_once(const Callback& callback) {
 }
 // _________________________________________________________________________________________________________________
 
+/* ####
+ */
 void draw(const int& x, std::ostream& out, size_t position) {
+    out << std::string(position, ' ') << x << std::endl;
+}
+
+void draw(const std::string& x, std::ostream& out, size_t position) {
     out << std::string(position, ' ') << x << std::endl;
 }
 
 class object_t {
 public:
-    object_t(int x) : self_(std::make_unique<int_model_t>(std::move(x))) { std::cout << "ctor" << std::endl; }
+    object_t(int x) : self_(std::make_unique<int_model_t>(std::move(x))) {
+        std::cout << "ctor" << std::endl;
+    }
+    object_t(std::string x) : self_(std::make_unique<string_model_t>(std::move(x))) {
+        std::cout << "ctor" << std::endl;
+    }
 
-    object_t(const object_t& x) : self_(std::make_unique<int_model_t>(*x.self_)) {}
-    object_t(object_t&&) noexcept = default;
+    object_t(const object_t& x) : self_(x.self_->copy_()) {} // copy constructor...
+    object_t(object_t&&) noexcept = default; // move constructor...
     object_t& operator=(const object_t& x) { return *this = object_t(x); }
     object_t& operator=(object_t&&) noexcept = default;
 
@@ -155,15 +166,33 @@ public:
     }
 
 private:
-    struct int_model_t {
+    struct concept_t {
+        virtual ~concept_t() = default;
+        virtual std::unique_ptr<concept_t> copy_() const = 0;
+        virtual void draw_(std::ostream&, size_t) const = 0;
+    };
+    struct int_model_t final : concept_t {
         int_model_t(int x) : data_(std::move(x)) {}
-        void draw_(std::ostream& out, size_t position) const {
+        std::unique_ptr<concept_t> copy_() const override {
+            return std::make_unique<int_model_t>(*this);
+        }
+        void draw_(std::ostream& out, size_t position) const override {
             draw(data_, out, position);
         }
         int data_;
     };
+    struct string_model_t final : concept_t {
+        string_model_t(std::string x) : data_(std::move(x)) {}
+        std::unique_ptr<concept_t> copy_() const override {
+            return std::make_unique<string_model_t>(*this);
+        }
+        void draw_(std::ostream& out, size_t position) const override {
+            draw(data_, out, position);
+        }
+        std::string data_;
+    };
 
-    std::unique_ptr<int_model_t> self_;
+    std::unique_ptr<concept_t> self_;
 };
 
 using document_t = std::vector<object_t>;
