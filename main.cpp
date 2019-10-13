e7925a423cd56ea5286ac52f710db5e8b3c0616f
aleksander <aleksander.mozetic@gmail.com>
2019-10-13 21:04:01 +0200

    Latest commit

--- main.cpp
+++ main.cpp
@@ -323,6 +323,7 @@ int main(int argc, char *argv[])
      * Time native: 0.000272 seconds
      * Time native: 0.000281 seconds
      * Time native: 0.000272 seconds
+     * Time native: 0.000272643 seconds
      */
     draw(document, std::cout, 0);
 
