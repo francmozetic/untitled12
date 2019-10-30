5ef21b82a8674813c3df0730c74d5b0b161bbd00
aleksander <aleksander.mozetic@gmail.com>
2019-10-30 15:51:22 +0100

    Latest commit

--- task.cpp
+++ task.cpp
@@ -1,6 +1,8 @@
 #include <condition_variable>
 #include <deque>
 #include <mutex>
+#include <thread>
+#include <vector>
 
 class notification_queue {
     std::deque<std::function<void()>> _q;
@@ -13,5 +15,28 @@ public:
         while (_q.empty()) _ready.wait(lock);
         x = std::move(_q.front());
         _q.pop_front();
+    }
+
+    template<typename F>
+    void push(F&& f) {
+        {
+            std::unique_lock<std::mutex> lock{_mutex};
+            _q.emplace_back(std::forward<F>(f));
+        }
+        _ready.notify_one();
+    }
+};
+
+class task_system {
+    const unsigned _count = std::thread::hardware_concurrency();
+    std::vector<std::thread> _threads;
+    notification_queue _q;
+
+    void run(unsigned i) {
+        while (true) {
+            std::function<void()> f;
+            _q.pop(f);
+            f();
+        }
     }
 };
