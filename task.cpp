f4511a2d4427c0da3e8696e9e5c6bad9319ce0fc
aleksander <aleksander.mozetic@gmail.com>
2019-10-30 21:00:51 +0100

    Latest commit

--- task.cpp
+++ task.cpp
@@ -1,9 +1,11 @@
+#include <atomic>
 #include <condition_variable>
 #include <deque>
 #include <mutex>
 #include <thread>
 #include <vector>
 
+// Building a task system
 class notification_queue {
     std::deque<std::function<void()>> _q;
     bool _done{false};
@@ -11,6 +13,14 @@ class notification_queue {
     std::condition_variable _ready;
 
 public:
+    void done() {
+        {
+            std::unique_lock<std::mutex> lock{_mutex};
+            _done = true;
+        }
+        _ready.notify_all();
+    }
+
     void pop(std::function<void()>& x) {
         std::unique_lock<std::mutex> lock{_mutex};
         while (_q.empty() && !_done) _ready.wait(lock);
@@ -28,16 +38,16 @@ public:
     }
 };
 
-// Building a task system
 class task_system {
     const unsigned _count{std::thread::hardware_concurrency()};
     std::vector<std::thread> _threads;
-    notification_queue _q;
+    std::vector<notification_queue> _q{_count};
+    std::atomic<unsigned> _index{0};
 
     void run(unsigned i) {
         while (true) {
             std::function<void()> f;
-            _q.pop(f);
+            _q[i].pop(f);
             f();
         }
     }
@@ -48,15 +58,20 @@ public:
             _threads.emplace_back([&, n](){ run(n); });
         }
     }
+
     ~task_system() {
         for (auto& e : _threads) {
             e.join();
+        }
+        for (auto& e : _q) {
+            e.done();
         }
     }
 
     template<typename F>
     void async(F&& f) {
-        _q.push(std::forward<F>(f));
+        auto i = _index++;
+        _q[i % _count].push(std::forward<F>(f));
     }
 
 
