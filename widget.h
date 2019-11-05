8d9e9f925e4797ac205a43d173440e08303ab402
aleksander <aleksander.mozetic@gmail.com>
2019-11-05 11:44:48 +0100

    Latest commit

--- widget.h
+++ widget.h
@@ -18,7 +18,9 @@ public:
     ~widget();
 
     widget(widget&&) = default;
+    widget(widget const& other) = delete;
     widget& operator=(widget&&) = default;
+    widget& operator=(widget const& other) = delete;
 
     int processTo(std::ifstream &wavFp);
     void do_internal_work();
@@ -36,7 +38,7 @@ struct wavHeader {
     /* "fmt" sub-chunk */
     uint8_t         fmt[4];             // FMT header
     uint32_t        Subchunk1Size;      // Size of the fmt chunk
-    uint16_t        AudioFormat;        // Audio format 1=PCM,6=mulaw,7=alaw,257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+    uint16_t        AudioFormat;        // Audio format 1=PCM, 6=mulaw, 7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
     uint16_t        NumOfChan;          // Number of channels 1=Mono 2=Stereo
     uint32_t        SamplesPerSec;      // Sampling Frequency in Hz
     uint32_t        bytesPerSec;        // bytes per second
